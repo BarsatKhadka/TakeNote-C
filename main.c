2ebf2856984598267a997ef3da40378cebd85b43
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-02 13:51:27 -0600

    add: read()

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char c;
+
+    //read is a system level thing. It takes (fd- file descriptor as first param , where to store (variable's address)- second parameter and byte per request
+    // as third param)
+
+    //Currently we are at cannonical mode in terminal , meaning the user input goes only when you press enter.
+    while(read(STDIN_FILENO, &c, 1) == 1);
     return 0;
 }
