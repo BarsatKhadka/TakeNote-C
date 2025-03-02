6aaadf41e3dab7ab61d807bdae07f2a21c2eaa45
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-02 17:31:43 -0600

    add: raw Mode

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <unistd.h>
+#include <termios.h>
 
-int main(void) {
+
+void enableRawMode() {
+
+    struct termios raw;
+
+    //get current terminal attribute
+    tcgetattr(STDIN_FILENO, &raw);
+
+    //disable echo 
+    raw.c_lflag &= ~(ECHO);
+
+    //set current terminal attribute
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  }
+
+int main() {
+
+    //raw vs cannonical mode , enabling raw mode.
+    enableRawMode();
+
     char c;
 
     //read is a system level thing. It takes (fd- file descriptor as first param , where to store (variable's address)- second parameter and byte per request
     // as third param)
 
     //Currently we are at cannonical mode in terminal , meaning the user input goes only when you press enter.
-    while(read(STDIN_FILENO, &c, 1) == 1);
+    while(read(STDIN_FILENO, &c, 1) == 1 );
     return 0;
 }
